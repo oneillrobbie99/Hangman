f4e2e4f1360bbd3f0eb19c682b00b583b36eff31
oneillrobbie99 <oneillrobbie99@gmail.com>
2022-02-22 19:11:55 +0000

    Added usable words which will filter out invalid words from dictionary and looped it so it will return until a usable word is found

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
+""" Import random words from the dictionary to the main .py """
+
 import random
 
 from dictionary import dictionary
 
-print(dictionary)
+""" Some words in the list contain a '-' or a ' ' 
+    which won't work in the game so they need to 
+    be skipped for a usable word"""
+
+def get_usable_word(dictionary):
+    word = random.choice(dictionary)
+    while '-' in word or ' ' in word:
+        word = random.choice(dictionary)
+
+    return word
+
+
